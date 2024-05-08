e318affd5807c9702197e579b7bb2833f9410382
mv <katena.maslova.03@mail.ru>
2024-05-08 09:15:40 +0300

    fix: remove inMemory realisation

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,4 +6,3 @@ INSERT INTO public.mpa
 (name)
 VALUES('G'), ('PG'), ('PG-13'), ('R'), ('NC-17');
 
---insert into FILMS (NAME, DURATION, RELEASE_DATE, DESCRIPTION, MPA_ID) VALUES ( 'n', 100, '2002-02-06', 'sdc', 1 );
